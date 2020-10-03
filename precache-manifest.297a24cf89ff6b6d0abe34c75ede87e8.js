9648e2a8abacab1e009c91d031be24c80e5b0901
akumar-UI <kumar@akumar.co.uk>
2020-10-03 03:02:04 +0100

    Updates

---
+++ precache-manifest.297a24cf89ff6b6d0abe34c75ede87e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0245118644290b48106b894d1228ffb4",
+    "url": "/green/index.html"
+  },
+  {
+    "revision": "dbd23077361b39c5b3a1",
+    "url": "/green/static/css/main.44d6aae7.chunk.css"
+  },
+  {
+    "revision": "9af0dab0c0b9c46941c1",
+    "url": "/green/static/js/2.3c328da7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/green/static/js/2.3c328da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd23077361b39c5b3a1",
+    "url": "/green/static/js/main.603d88c2.chunk.js"
+  },
+  {
+    "revision": "1cd7ef218f03e09b9b84",
+    "url": "/green/static/js/runtime-main.2f1407ab.js"
+  },
+  {
+    "revision": "de757dc9a0ee3240327896ce11f48b0f",
+    "url": "/green/static/media/check.de757dc9.svg"
+  },
+  {
+    "revision": "a563635d2c187f9399dce23d8f00d585",
+    "url": "/green/static/media/gotham.a563635d.otf"
+  },
+  {
+    "revision": "9da97edd306f798b1cb2eb6dca334e22",
+    "url": "/green/static/media/logo.9da97edd.svg"
+  }
+]);
